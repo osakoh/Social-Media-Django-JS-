182307c1865a89a62efcaacbfee0b982c972a105
osakoh <macmive@gmail.com>
2021-02-15 23:00:17 +0000

    created post_save signal

---
+++ profiles/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def post_save_create_profile(sender, instance, created, **kwargs):
+    # print(f"sender: {sender}\ninstance: {instance}") #
+    # sender: <class 'django.contrib.auth.models.User'>
+    # instance: test2
+
+    if created:
+        Profile.objects.create(user=instance)
